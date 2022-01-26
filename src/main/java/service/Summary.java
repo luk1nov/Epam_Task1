d1fedfb25d65142be9693b784ab7a220b81b2743
Anton <lukyanoff1337@gmail.com>
2022-01-26 11:39:51 +0300

    added methods

--- src/main/java/service/Summary.java
+++ src/main/java/service/Summary.java
@@ -1,4 +1,22 @@
 package service;
 
+import entity.ArrayEntity;
+
 public class Summary {
+
+    public int findMinValue(ArrayEntity arrayEntity){
+        int min = arrayEntity.getFirstElement();
+        for (int i: arrayEntity.getArray()) {
+            if(i < min) min = i;
+        }
+        return min;
+    }
+
+    public int findMaxValue(ArrayEntity arrayEntity){
+        int max = arrayEntity.getFirstElement();
+        for (int i: arrayEntity.getArray()) {
+            if(i > max) max = i;
+        }
+        return max;
+    }
 }
