70e6166ec6bfcf048f93c52a2ee6c812a40d7a85
Anton <lukyanoff1337@gmail.com>
2022-01-26 11:40:08 +0300

    updated constructor

--- src/main/java/entity/ArrayEntity.java
+++ src/main/java/entity/ArrayEntity.java
@@ -1,5 +1,25 @@
 package entity;
 
 public class ArrayEntity {
+    protected int[] array;
 
+    public ArrayEntity(int[] array) {
+        this.array = array;
+    }
+
+    public ArrayEntity() {
+        this.array = new int[]{1, 2, 3, 10, 7, 5};
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array;
+    }
+
+    public int getFirstElement(){
+        return array[0];
+    }
 }
