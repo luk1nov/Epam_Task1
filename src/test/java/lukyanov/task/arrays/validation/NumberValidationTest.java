783aea8b91b1fda528aa50dfc1208679a42f4a8f
Anton <lukyanoff1337@gmail.com>
2022-02-07 02:18:18 +0300

    commit

--- src/test/java/lukyanov/task/arrays/validation/NumberValidationTest.java
+++ src/test/java/lukyanov/task/arrays/validation/NumberValidationTest.java
@@ -3,17 +3,25 @@ package lukyanov.task.arrays.validation;
 import lukyanov.task.arrays.exception.CustomException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberValidationTest {
 
-    @Test
-    @DisplayName("validateLine")
-    public void validateLine() {
-        assertTrue(NumberValidation.validateLine("1;4;5"));
-        assertTrue(NumberValidation.validateLine("1; 4; 5;"));
-        assertFalse(NumberValidation.validateLine("1 ; 4d ; 5;"));
+    @ParameterizedTest
+    @ValueSource(strings = {"1;4;5", "1; 4; 5;"})
+    @DisplayName("validateValidLine")
+    void validateValidLine(String input) {
+        assertTrue(NumberValidation.validateLine(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1w;4;5-", "2;we;643"})
+    @DisplayName("validateInvalidLine")
+    void validateInvalidLine(String input) {
+        assertFalse(NumberValidation.validateLine(input));
     }
 
 }
